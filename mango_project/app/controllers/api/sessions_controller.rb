04199cddf950fb7dfe23c850ab5c850b520f206e
Chubbibanana <dongsoo.tchah@gmail.com>
2021-02-08 14:20:37 -0800

    Session AJAX requests are working

--- mango_project/app/controllers/api/sessions_controller.rb
+++ mango_project/app/controllers/api/sessions_controller.rb
@@ -1,2 +1,21 @@
 class Api::SessionsController < ApplicationController
+    skip_before_action :verify_authenticity_token
+
+    def create
+        @user = User.find_by_credentials(
+            params[:user][:email],
+            params[:user][:password]
+        )
+        if @user
+            login(@user)
+            render 'api/users/show';
+        else
+            render json: ["Your Mango ID or password was incorrect."], status: 422
+        end
+    end
+
+    def destroy
+        logout
+        render json: ["Success"]
+    end
 end
