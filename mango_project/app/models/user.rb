f14091ae1d309cdd7fe517163a71cc5017a48aee
Chubbibanana <dongsoo.tchah@gmail.com>
2021-02-08 14:07:53 -0800

    User Creation works!

--- mango_project/app/models/user.rb
+++ mango_project/app/models/user.rb
@@ -2,14 +2,14 @@ class User < ApplicationRecord
     validates :email, :session_token, presence: true, uniqueness: true
     validates :password, length: {minimum: 6}, allow_nil: true
 
-    attr_reader: password
+    attr_reader :password
 
     # SPIRE
 
     after_initialize :ensure_session_token
 
-    def self.find_by_credentials(username, password)
-        user = User.find_by(username: username)
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
 
         if user && user.is_password?(password)
             user
