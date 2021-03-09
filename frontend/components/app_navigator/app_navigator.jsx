dc2ecf7609fa8028665939c5bcc01df6c45612d5
Chubbibanana <dongsoo.tchah@gmail.com>
2021-03-08 23:49:15 -0800

    Plan for search

--- frontend/components/app_navigator/app_navigator.jsx
+++ frontend/components/app_navigator/app_navigator.jsx
@@ -10,6 +10,8 @@ const AppNavigator = () => {
         <nav className="app-nav">
             <p className="mango-logo"><img className="small-logo" src={window.mangoURL} alt="Mango Logo" /><Link to='/'>Music</Link></p>
             <input className="nav-search" type="text" placeholder="Search"/>
+            {/* find a way to parse through state */}
+            {/* maybe in mstp? */}
             {/* <button>Listen Now</button> */}
             <button><Link to="/browse" className="link">Browse</Link></button>
             {/* <button>Radio</button> */}
